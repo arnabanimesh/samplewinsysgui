7fdf281c59b3b36be723ef189a4646e9ce378a5f
Arnab Animesh Das <arnabanimesh.a@gmail.com>
2022-04-10 18:50:23 +0530

    Age text generation app

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ use windows_sys::{
     Win32::UI::WindowsAndMessaging::*,
 };
 
-const NULL: usize = 0;
 const FILE_MENU_NEW: usize = 1;
+// const FILE_MENU_OPEN: usize = 2;
 const FILE_MENU_EXIT: usize = 3;
-const CHANGE_TITLE: usize = 4;
+const GENERATE_BUTTON: usize = 4;
 
 static mut H_MENU: HMENU = 0;
-static mut H_EDIT: HWND = 0;
+static mut H_NAME: HWND = 0;
+static mut H_AGE: HWND = 0;
+static mut H_OUT: HWND = 0;
 
-macro_rules! wide_str {
+macro_rules! wide_str_ptr {
     ($str:expr) => {
         $str.encode_utf16()
             .chain(Some(0))
@@ -33,7 +35,7 @@ fn main() {
             hbrBackground: COLOR_WINDOW as isize,
             hCursor: LoadCursorW(0, IDC_ARROW),
             hInstance: hinst,
-            lpszClassName: wide_str!(window_class),
+            lpszClassName: wide_str_ptr!(window_class),
             lpfnWndProc: Some(window_procedure),
             ..zeroed()
         };
@@ -42,8 +44,8 @@ fn main() {
         }
         CreateWindowExW(
             0,
-            wide_str!(window_class),
-            wide_str!(window_name),
+            wide_str_ptr!(window_class),
+            wide_str_ptr!(window_name),
             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
             100,
             100,
@@ -73,10 +75,31 @@ unsafe extern "system" fn window_procedure(
             match wp {
                 FILE_MENU_EXIT => DestroyWindow(hwnd),
                 FILE_MENU_NEW => MessageBeep(MB_ICONINFORMATION),
-                CHANGE_TITLE => {
-                    let mut text: [u16; 100] = [0; 100]; // max array length should be limited to i32::MAX
-                    GetWindowTextW(H_EDIT, text.as_mut_ptr(), text.len() as i32);
-                    SetWindowTextW(hwnd, text.as_ptr())
+                GENERATE_BUTTON => {
+                    let (mut name, mut age, mut out): ([u16; 30], [u16; 10], [u16; 50]) =
+                        ([0; 30], [0; 10], [0; 50]);
+                    GetWindowTextW(H_NAME, name.as_mut_ptr(), name.len() as i32);
+                    GetWindowTextW(H_AGE, age.as_mut_ptr(), age.len() as i32);
+                    let mut i = 0;
+                    while name[i] != 0 {
+                        out[i] = name[i];
+                        i += 1;
+                    }
+                    for x in " is ".encode_utf16() {
+                        out[i] = x;
+                        i += 1;
+                    }
+                    let mut j = 0;
+                    while age[j] != 0 {
+                        out[i] = age[j];
+                        j += 1;
+                        i += 1;
+                    }
+                    for x in " years old.".encode_utf16() {
+                        out[i] = x;
+                        i += 1;
+                    }
+                    SetWindowTextW(H_OUT, out.as_ptr())
                 }
                 _ => 0,
             };
@@ -95,45 +118,45 @@ unsafe fn add_menus(hwnd: HWND) {
     H_MENU = CreateMenu();
     let file_menu = CreateMenu();
     let sub_menu = CreateMenu();
-    AppendMenuW(sub_menu, MF_STRING, NULL, wide_str!("SubMenu Item"));
-    AppendMenuW(file_menu, MF_STRING, FILE_MENU_NEW, wide_str!("New"));
+    AppendMenuW(sub_menu, MF_STRING, 0, wide_str_ptr!("SubMenu Item"));
+    AppendMenuW(file_menu, MF_STRING, FILE_MENU_NEW, wide_str_ptr!("New"));
     AppendMenuW(
         file_menu,
         MF_POPUP,
         sub_menu as usize,
-        wide_str!("Open SubMenu"),
+        wide_str_ptr!("Open SubMenu"),
     );
-    AppendMenuW(file_menu, MF_SEPARATOR, NULL, null());
-    AppendMenuW(file_menu, MF_STRING, FILE_MENU_EXIT, wide_str!("Exit"));
-    AppendMenuW(H_MENU, MF_POPUP, file_menu as usize, wide_str!("File"));
-    AppendMenuW(H_MENU, MF_STRING, NULL, wide_str!("Help"));
+    AppendMenuW(file_menu, MF_SEPARATOR, 0, null());
+    AppendMenuW(file_menu, MF_STRING, FILE_MENU_EXIT, wide_str_ptr!("Exit"));
+    AppendMenuW(H_MENU, MF_POPUP, file_menu as usize, wide_str_ptr!("File"));
+    AppendMenuW(H_MENU, MF_STRING, 0, wide_str_ptr!("Help"));
     SetMenu(hwnd, H_MENU);
 }
 
 unsafe fn add_controls(hwnd: HWND) {
     CreateWindowExW(
         0,
-        wide_str!("static"),
-        wide_str!("Enter Text Here :"),
-        WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER as u32,
-        200,
-        100,
+        wide_str_ptr!("Static"),
+        wide_str_ptr!("Name :"),
+        WS_VISIBLE | WS_CHILD,
         100,
         50,
+        98,
+        38,
         hwnd,
         0,
         0,
         null(),
     );
-    H_EDIT = CreateWindowExW(
+    H_NAME = CreateWindowExW(
         0,
-        wide_str!("Edit"),
-        wide_str!("..."),
-        WS_VISIBLE | WS_CHILD | WS_BORDER | (ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL) as u32,
+        wide_str_ptr!("Edit"),
+        null(),
+        WS_VISIBLE | WS_CHILD | WS_BORDER,
         200,
-        152,
-        100,
         50,
+        98,
+        38,
         hwnd,
         0,
         0,
@@ -141,15 +164,57 @@ unsafe fn add_controls(hwnd: HWND) {
     );
     CreateWindowExW(
         0,
-        wide_str!("Button"),
-        wide_str!("Change Title"),
+        wide_str_ptr!("Static"),
+        wide_str_ptr!("Age :"),
         WS_VISIBLE | WS_CHILD,
-        200,
-        204,
         100,
-        50,
+        90,
+        98,
+        38,
         hwnd,
-        CHANGE_TITLE as isize,
+        0,
+        0,
+        null(),
+    );
+    H_AGE = CreateWindowExW(
+        0,
+        wide_str_ptr!("Edit"),
+        null(),
+        WS_VISIBLE | WS_CHILD | WS_BORDER,
+        200,
+        90,
+        98,
+        38,
+        hwnd,
+        0,
+        0,
+        null(),
+    );
+    CreateWindowExW(
+        0,
+        wide_str_ptr!("Button"),
+        wide_str_ptr!("Generate"),
+        WS_VISIBLE | WS_CHILD,
+        150,
+        140,
+        98,
+        38,
+        hwnd,
+        GENERATE_BUTTON as isize,
+        0,
+        null(),
+    );
+    H_OUT = CreateWindowExW(
+        0,
+        wide_str_ptr!("Edit"),
+        null(),
+        WS_VISIBLE | WS_CHILD | WS_BORDER,
+        100,
+        200,
+        300,
+        200,
+        hwnd,
+        0,
         0,
         null(),
     );
